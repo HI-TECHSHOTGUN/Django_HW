c9125654b350985884d0fe492526e91ec10610a1
HI-TECHSHOTGUN <maksimov_maksime@mail.ru>
2025-06-24 05:33:11 +0300

    base

---
+++ users/models.py
@@ -0,0 +1,36 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your models here.
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name="Email")
+    phone_number = models.CharField(
+        max_length=25,
+        verbose_name="Телефон",
+        blank=True,
+        null=True,
+        help_text="Введите номер телефона",
+    )
+    country = models.CharField(
+        max_length=50,
+        verbose_name="Страна",
+        help_text="Введите страну",
+    )
+    avatar = models.ImageField(
+        upload_to="users/avatars/",
+        blank=True,
+        null=True,
+        help_text="Загрузите аватар",
+    )
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.email
---
+++ users/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views.generic import CreateView
+
+
+# Create your views here.
+
+
+class RegisterView(CreateView):
+    template_name =
+
+
