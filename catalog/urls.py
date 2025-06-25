131f1c6a982b258d684e6a95226500e091e34189
HI-TECHSHOTGUN <maksimov_maksime@mail.ru>
2025-06-25 03:27:24 +0300

    help me

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import ProductListView, ContactView, ProductDetailView, ProductCreateView, ProductUpdateView, \
-    ProductDeleteView
+    ProductDeleteView, UnpublishProduct
 
 app_name = 'catalog'
 
@@ -11,4 +11,5 @@ urlpatterns = [
     path('new/', ProductCreateView.as_view(), name='new_product'),
     path('details/update/<int:pk>/', ProductUpdateView.as_view(), name='update_product'),
     path('details/delete/<int:pk>/', ProductDeleteView.as_view(), name='delete_product'),
+    path('unpublish/<int:pk>/', UnpublishProduct.as_view(), name='unpublish')
 ]
